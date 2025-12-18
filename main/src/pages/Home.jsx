488695151a00bcc1eb2b81399adc62462e14d706
amr <amrgabr863@gmail.com>
2025-12-18 17:33:05 +0200

    first commit

---
+++ main/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section className="home">
+    
+    
+      <h1 className="title">START FRAMEWORK</h1>
+      <div className="star">⭐</div>
+      <p>Graphic Artist - Web Designer - Illustrator</p>
+    </section>
+  );
+}
